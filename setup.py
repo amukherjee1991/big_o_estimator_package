ef1d4bc1cb4da891fa64f375d581e0f3f2c72786
amukherjee1991 <arghyamukherjee1991@gmail.com>
2024-09-18 13:11:19 -0500

    0.0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="big_o_estimator",  # Name of your package
-    version="0.1.0",  # Initial release version
+    version="0.1.1",  # Initial release version
     description="A package to estimate Big-O complexity of Python functions.",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",  # If your README is in Markdown
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/big_o_estimator",  # GitHub repo or project homepage
+    author="Arghya ",
+    author_email="arghyazzz@gmail.com",
+    url="https://github.com/amukherjee1991/big_o_estimator_package",  # GitHub repo or project homepage
     packages=find_packages(),  # Automatically find and include the package
     classifiers=[
         "Programming Language :: Python :: 3",
